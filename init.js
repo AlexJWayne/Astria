b089ec858fec85de6652ac6c3e524b8c53d3e1d0
Alex Wayne <alex@beautifulpixel.com>
2011-05-16 23:26:09 -0700

    Use sinatra and compile on the fly liek a champ.

--- init.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  head.js('lib/three.js', 'lib/RequestAnimationFrame.js', 'lib/stats.js', 'lib/underscore.js', 'support/math.js', 'support/animator.js', 'support/orbit_camera.js', 'support/vector3.js', 'game/game.js', 'game/artifact.js', 'game/artifacts/test1.js', function() {
-    Game.init();
-    return Game.animate();
-  });
-}).call(this);
